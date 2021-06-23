5d6bf54249121ec1b652c029e2d3af1d1c20df54
Steve Burket <burketsteve@gmail.com>
2021-06-23 16:14:38 -0500

    added pet owner controller

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -12,14 +12,44 @@ namespace pet_hotel.Controllers
     public class PetOwnersController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
+        public PetOwnersController(ApplicationContext context)
+        {
             _context = context;
         }
+
+        //GET api/petowners all pet owners
+
+            [HttpGet] // GET /api/petOwners/
+        public IEnumerable<PetOwner> GetPetOwners() {
+            return _context.PetOwners;
+               
+        }
+
+        // //GET owners by ID
+        // [HttpGet("{id}")]
+        // public IActionResult GetById(int id)
+        // {
+        //     PetOwner owner = _context.PetOwners;
+        //     if (owner == null)
+        //     {
+        //         return NotFound();
+
+        //     }
+        //     return Ok(owner);
+        // }
+
+    // [HttpPost]
+    // public IActionResult CreatePetOwner([FromBody] PetOwner owner) {
+    //     _context.Add(owner);
+    //     _context.SaveChanges();
+    //     return CreatedAtAction(nameof(GetById), owner, owner);
+    // }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
+        public IEnumerable<PetOwner> GetPets()
+        {
             return new List<PetOwner>();
         }
     }
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,47 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType {
+        Basset,
+        Shepherd,
+        Poodle,
+        Beagle,
+        Bulldog,
+        Terrier,
+        Boxer,
+        Labrador,
+        Retriever
+
+    }
+    public enum PetColorType {
+        White,
+        Black,
+        Golden,
+        Tricolor,
+        Spotted
+
+    }
+    public class Pet {
+        public int id {get; set;}
+
+        [Required]
+        public string name {get; set;}
+        
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetBreedType breed {get; set;}
+
+        [Required]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public PetColorType color {get; set;}
+    
+        public Nullable<DateTime> checkedInAt {get; set;}
+
+        [Required]
+        public int petOwnerid {get; set;}
+
+        public PetOwner petOwner {get; set;}
+
+
+    }
 }
