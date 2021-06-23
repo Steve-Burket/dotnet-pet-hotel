bae29dcd93f09c173118ce2b783e6a835abadad8
Steve Burket <burketsteve@gmail.com>
2021-06-23 14:14:15 -0500

    initial commit; pet owner model created

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,29 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner
+    {
+        public int id { get; set; }
+
+        // name
+        [Required]
+        public string name { get; set; }
+        
+        // email validated format name@email.com
+        [Required]
+        public string emailAddress { get; set; } 
+
+        // pet count 
+        [NotMapped]
+        public int petCount { get; set; }
+    }
 }
+
+// public int breadCount
+// {
+//     get
+//     {
+//         // if breads is not populated, assume 0
+//         if (this.breads == null) return 0;
+//         return this.breads.Count;
+//     }
